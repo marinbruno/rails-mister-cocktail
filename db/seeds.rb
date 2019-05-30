d671f1230fb9aaf7c5c35ac19167e6aa723d17ec
Bruno <bruno_marin@hotmail.com>
2019-05-30 15:05:34 -0300

    DB and Models validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_json = open(url).read
+cocktails = JSON.parse(cocktails_json)
+
+cocktails_array = cocktails["drinks"].values
+
+cocktails_array.each do |cocktail|
+  Cocktail.create(cocktail)
+end
