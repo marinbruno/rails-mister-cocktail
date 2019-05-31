c5c992d7c46dceabf639978f7d6b4e93893b310b
Bruno <bruno_marin@hotmail.com>
2019-05-31 12:13:42 -0300

    fixed db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,25 @@
 require 'open-uri'
 require 'json'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-cocktails_json = open(url).read
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_json = open(ingredients_url).read
+ingredients = JSON.parse(ingredients_json)
+
+ingredients_array = ingredients["drinks"]
+
+ingredients_array.each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+
+cocktails_url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+cocktails_json = open(cocktails_url).read
 cocktails = JSON.parse(cocktails_json)
 
 cocktails_array = cocktails["drinks"]
 
 cocktails_array.each do |cocktail|
-  Cocktail.create!(name: cocktail["strIngredient1"])
+  Cocktail.create!(name: cocktail["strDrink"])
 end
