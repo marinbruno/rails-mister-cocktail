517121f9916dc8fcf47bd6a2a52479cb49ba092b
Bruno <bruno_marin@hotmail.com>
2019-05-31 16:11:00 -0300

    uploader fix

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -12,4 +12,5 @@ class PhotoUploader < CarrierWave::Uploader::Base
   version :bright_face do
     cloudinary_transformation effect: "brightness:30", radius: 20,
       width: 150, height: 150, crop: :thumb, gravity: :face
+  end
 end
